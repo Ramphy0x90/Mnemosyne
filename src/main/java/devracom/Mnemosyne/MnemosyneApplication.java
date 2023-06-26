3a390a1c256b98f913442ede92944a243e926e43
ramphy0x90 <ramphy11@hotmail.com>
2023-06-26 21:27:10 +0200

    Update

--- src/main/java/devracom/Mnemosyne/MnemosyneApplication.java
+++ src/main/java/devracom/Mnemosyne/MnemosyneApplication.java
@@ -1,5 +1,7 @@
 package devracom.Mnemosyne;
 
+import devracom.Mnemosyne.services.FileService;
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
